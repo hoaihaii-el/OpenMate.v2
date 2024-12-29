41b8401075304be95fb1ad427cdcfcfa17e17133
hoaihaii-el <hoaihai.el@gmail.com>
2024-12-29 13:24:37 +0700

    [API] restructure APIs

---
+++ OpenMate.APIs/OpenMate.API.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenMate.API.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public string? Username { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? DisplayName { get; set; }
+        public string? MainRole { get; set; }
+        public string? SkillSet { get; set; }
+    }
+}
---
+++ OpenMate.APIs/OpenMate.API.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenMate.API.Domain.Entities;
+
+namespace OpenMate.API.Infrastructure.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
