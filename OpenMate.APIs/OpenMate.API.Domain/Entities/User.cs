8a0a622ce2c093ae8a6f7375861eb43131beb61f
hoaihaii-el <hoaihai.el@gmail.com>
2024-12-29 14:29:27 +0700

    [API] change Console App to Library App

--- OpenMate.APIs/OpenMate.API.Domain/Entities/User.cs
+++ OpenMate.APIs/OpenMate.API.Domain/Entities/User.cs
@@ -5,10 +5,13 @@ namespace OpenMate.API.Domain.Entities
     public class User
     {
         [Key]
+        [MaxLength(10)]
         public string? Username { get; set; }
         public string? PasswordHash { get; set; }
+        [MaxLength(50)]
         public string? DisplayName { get; set; }
         public string? MainRole { get; set; }
+        [MaxLength(200)]
         public string? SkillSet { get; set; }
     }
 }
---
+++ OpenMate.APIs/OpenMate.API.Infrastructure/Migrations/20241229071306_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenMate.API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DisplayName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    MainRole = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SkillSet = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Username);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
